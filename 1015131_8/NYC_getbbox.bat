4e5d898fc1694f51da4af04a18f1f3c697df5bcc
Irmak Turan <irmakturan@gmail.com>
2019-12-12 10:32:28 -0500

    191211 UPDATES

--- 1015131_8/NYC_getbbox.bat
+++
@@ -1,4 +0,0 @@
-
-for %%a in (.) do set ID=%%~nxa
-getbbox %ID%.rad > bbox.txt
-::pause
--- 1015131_8/NYC_oconv.bat
+++
@@ -1,9 +0,0 @@
-@REM Creating octree for NYC simulation 
-
-SET RAYPATH=.;C:\DIVA\Radiance\lib;C:\DIVA\Radiance\bin_64;C:\DIVA\DaysimBinaries;
-SET PATH=.;C:\DIVA\Radiance\lib;C:\DIVA\Radiance\bin_64;C:\DIVA\DaysimBinaries;
-
-D:
-for %%a in (.) do set current=%%~nxa
-oconv materials.rad %current%.rad %current%_ground.rad > %current%.oct
-::PAUSE
